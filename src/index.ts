1bd2564824d0c84ed489ceaefc44d450c5296e1a
deane <deannacorado@gmail.com>
2026-03-17 23:23:54 +0800

    updated port fallback

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,16 @@
-import "dotenv/config";
-import app from "./app";
-import sequelize from "./config/db";
+import 'dotenv/config';
+import app from './app';
+import sequelize from './config/db';
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 4000;
 
 sequelize
   .sync()
   .then(() => {
-    console.log("Database connected successfully");
+    console.log('Database connected successfully');
 
     app.listen(port, () => {
       console.log(`Server is running on port ${port}`);
     });
   })
-  .catch((err) => console.error("DB error:", err));
+  .catch((err) => console.error('DB error:', err));
